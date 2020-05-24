30d31fef6cab936396652d8fc53bb41d359d9436
Zana H <zhertica@gmail.com>
2020-05-24 02:42:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |name, index|
-    puts "#{index+1}. #{name}"
+    puts "#{index+1}. "
   end
 end
